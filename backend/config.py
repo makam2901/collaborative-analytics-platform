ec807db7fb5ece6adde260995b3b619a20cce8b0
Makam Sri Manikesh <manikeshmakam@gmail.com>
2025-07-31 16:47:06 -0400

    fix(ci): use conditional dotenv loading and provide secrets

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from the .env file in the project root
-load_dotenv()
+if os.getenv("CI") is None:
+    load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
